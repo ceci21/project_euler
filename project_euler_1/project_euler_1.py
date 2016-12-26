686d14989c260ad50d9cccd4df4c47a83747a2d1
Ceci <ceci.goss@gmail.com>
2016-12-25 20:28:20 -0600

    Added python solution to problem 1

--- project_euler_1/project_euler_1.py
+++ project_euler_1/project_euler_1.py
@@ -1,12 +1,12 @@
 def challenge_1():
     sum = 0
-    for n in range(0, 1000, 1):
+    for n in range(0, 1000):
         if n % 15 == 0:
-            sum = sum + n
+            sum += n
         elif n % 5 == 0:
-            sum = sum + n
+            sum += n
         elif n % 3 == 0:
-            sum = sum + n
+            sum += n
     print(sum)
 
 challenge_1()
